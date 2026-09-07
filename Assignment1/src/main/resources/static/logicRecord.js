9c8957f67213513a4489cdfa79207391fcbad046
nhandang1304 <yandang1304@gmail.com>
2026-09-07 11:29:04 +0930

    Add stopRecord() function

--- Assignment1/src/main/resources/static/logicRecord.js
+++ Assignment1/src/main/resources/static/logicRecord.js
@@ -1,11 +1,26 @@
 const constraintRecord = {audio: true};
 let audioRecorder;
 let recordButton = document.querySelector("#recordButton");
+let isRecording = false;
 let chunks = [];
+recordButton.addEventListener("click", startRecord()=>{
+	if 
+})
+
 async function startRecord(){
 	const permittedStream = await navigator.mediaDevices.getUserMedia(constraintRecord);
 	audioRecorder = new MediaRecorder(permittedStream);
 	audioRecorder.ondataavailable = (event) => { chunks.push(event.data)};
 	audioRecorder.start();	
+	
+}
+
+async function stopRecord(){
+	audioRecorder.stop();
+	audioRecorder.onstop = ()=> {
+		const blobAudio = new Blob(chunks, {type: "audio/webm"});
+	};
+	const url = URL.createObjectURL(blobAudio);
+	console.log("audioURL");
 	
 }
