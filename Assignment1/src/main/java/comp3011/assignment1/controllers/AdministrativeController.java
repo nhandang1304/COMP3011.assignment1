e32d43493fc4a28e13a414a7777009644e30d5a5
nhandang1304 <yandang1304@gmail.com>
2026-09-08 21:06:52 +0930

    Create GET endpoint for Retrieving global token usage statistics

--- Assignment1/src/main/java/comp3011/assignment1/controllers/AdministrativeController.java
+++ Assignment1/src/main/java/comp3011/assignment1/controllers/AdministrativeController.java
@@ -7,17 +7,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import comp3011.assignment1.models.ServerUptimeResponse;
+import comp3011.assignment1.models.GlobalStatsResponse;
 import comp3011.assignment1.models.ServerShutdownResponse;
+import comp3011.assignment1.services.GlobalStatService;
 @RestController
-@RequestMapping("/api/v1/admin")
+@RequestMapping("/api/v1")
 public class AdministrativeController {
 	private final Instant serverStartTime = Instant.now();
 	
-	@GetMapping("/uptime")
+	@GetMapping("/admin/uptime")
 	public ServerUptimeResponse getServerUptime() {
 		Instant currentTimeResponse = Instant.now();
 		double serverUptimeSeconds = Duration.between(serverStartTime, currentTimeResponse).toMillis();
 		return new ServerUptimeResponse(serverStartTime, currentTimeResponse, serverUptimeSeconds);
 	}
-	
+	@GetMapping("/global/stats")
+	public GlobalStatsResponse getGlobalStat(GlobalStatService global) {
+		long inputTokens = global.getInputTokens();
+		long outputTokens = global.getOutputTokens();
+		return new GlobalStatsResponse(inputTokens, outputTokens);
+	}
 }
