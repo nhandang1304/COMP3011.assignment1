a82b2f3450a6d254f2ad4c919d8a0a4b5f76f07b
nhandang1304 <yandang1304@gmail.com>
2026-09-08 15:08:07 +0930

    Configure RestClient to call openAI

---
+++ Assignment1/src/main/java/comp3011/assignment1/config/RestClientConfig.java
@@ -0,0 +1,14 @@
+package comp3011.assignment1.config;
+
+import org.springframework.web.client.RestClient;
+import org.springframework.context.annotation.*;
+
+@Configuration
+public class RestClientConfig {
+	@Bean
+	public RestClient RestClientConfig() {
+		return RestClient.builder().baseUrl("https://api.openai.com").build();
+								.
+						
+	}
+}
