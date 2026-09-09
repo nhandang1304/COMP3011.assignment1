5bb56ae6b1aa236afeab1418798ffe3cb32ec543
nhandang1304 <yandang1304@gmail.com>
2026-09-09 13:58:00 +0930

    Creating shutdown request (only accepted request)

--- Assignment1/src/main/java/comp3011/assignment1/controllers/AdministrativeController.java
+++ Assignment1/src/main/java/comp3011/assignment1/controllers/AdministrativeController.java
@@ -1,19 +1,28 @@
 package comp3011.assignment1.controllers;
 
+
 import java.time.Duration;
 import java.time.Instant;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import comp3011.assignment1.models.ServerUptimeResponse;
 import comp3011.assignment1.models.GlobalStatsResponse;
 import comp3011.assignment1.models.ServerShutdownResponse;
 import comp3011.assignment1.services.GlobalStatService;
+
 @RestController
 @RequestMapping("/api/v1")
 public class AdministrativeController {
 	private final Instant serverStartTime = Instant.now();
+	private final ConfigurableApplicationContext appContext;
+	public AdministrativeController(ConfigurableApplicationContext appContext) {
+		this.appContext = appContext;
+	}
 	
 	@GetMapping("/admin/uptime")
 	public ServerUptimeResponse getServerUptime() {
@@ -21,5 +30,14 @@ public class AdministrativeController {
 		double serverUptimeSeconds = Duration.between(serverStartTime, currentTimeResponse).toMillis() / 1000.0;
 		return new ServerUptimeResponse(serverStartTime, currentTimeResponse, serverUptimeSeconds);
 	}
-	
+	@PostMapping("/admin/shutdown")
+	public ResponseEntity shutdown() throws InterruptedException {
+		ServerShutdownResponse shutdownMessage = new ServerShutdownResponse("Graceful shutdown requested.");
+		
+		Runnable shutdownTask = ()-> appContext.close();
+		Thread thread = new Thread(shutdownTask);
+		thread.sleep(1000);
+		thread.start();
+		return ResponseEntity.status(HttpStatus.ACCEPTED).body(shutdownMessage);
+	}
 }
--- Assignment1/src/main/java/comp3011/assignment1/models/AudioTranscriptionResponse.java
+++ Assignment1/src/main/java/comp3011/assignment1/models/AudioTranscriptionResponse.java
@@ -2,15 +2,18 @@ package comp3011.assignment1.models;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 
-record TokenUsage(
-		@JsonProperty("input_tokens")
-		long inputTokens, 
-		@JsonProperty("output_tokens")
-		long outputTokens) {}
+
 		
 public record AudioTranscriptionResponse(
 		String text,
-		TokenUsage usage) {}
+		TokenUsage usage) {
+	
+	public record TokenUsage(
+			@JsonProperty("input_tokens")
+			long inputTokens, 
+			@JsonProperty("output_tokens")
+			long outputTokens) {}
+}
 		
 
 
