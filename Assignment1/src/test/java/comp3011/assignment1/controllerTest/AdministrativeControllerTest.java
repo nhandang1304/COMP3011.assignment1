35e5b90c399da9febced53ba14d95beb80badbae
nhandang1304 <yandang1304@gmail.com>
2026-09-13 19:57:28 +0930

    Adding testShutdownSuccessfulResponse() and testShutdownUnsuccessfulResponse()

--- Assignment1/src/test/java/comp3011/assignment1/controllerTest/AdministrativeControllerTest.java
+++ Assignment1/src/test/java/comp3011/assignment1/controllerTest/AdministrativeControllerTest.java
@@ -3,13 +3,16 @@ package comp3011.assignment1.controllerTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.multipart.MultipartFile;
 import comp3011.assignment1.services.*;
 import comp3011.assignment1.controllers.AdministrativeController;
-import comp3011.assignment1.models.ServerUptimeResponse;
+import comp3011.assignment1.models.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -24,13 +27,28 @@ public class AdministrativeControllerTest {
 		appContext = mock(ConfigurableApplicationContext.class);
 		adminController = new AdministrativeController(appContext);
 	}
+	@Test
 	void testValidServerUptimeResponse() {
-		ServerUptimeResponse response = adminController.getServerUptime();
-		assertNotNull(response.utcServerStart());
-		assertNotNull(response.utcNow());
-		assertTrue(response.serverUptimeSeconds() >= 0);
+		ServerUptimeResponse uptimeResponse = adminController.getServerUptime();
+		assertNotNull(uptimeResponse.utcServerStart());
+		assertNotNull(uptimeResponse.utcNow());
+		assertTrue(uptimeResponse.serverUptimeSeconds() >= 0);
 	}
-	void testValidShutdownResponse() {
+	@Test
+	void testShutdownSuccessfulResponse() {
+		ResponseEntity<?> shutdownResponse = adminController.shutdown();
+		assertEquals(HttpStatus.ACCEPTED, shutdownResponse.getStatusCode());
+        assertInstanceOf(ServerShutdownResponse.class, shutdownResponse.getBody());
+        assertEquals("Graceful shutdown requested.", ((ServerShutdownResponse) shutdownResponse.getBody()).message());
 		
+	}
+	@Test 
+	void testShutdownUnsuccessfulResponse() {
+		adminController.shutdown();
+		ResponseEntity<?> shutdownResponse = adminController.shutdown();
+		assertEquals(HttpStatus.CONFLICT, shutdownResponse.getStatusCode());
+		ErrorsResponse error = (ErrorsResponse) shutdownResponse.getBody();
+        assertEquals(409, error.status());
+        assertEquals("/api/v1/admin/shutdown", error.path());
 	}
 }
